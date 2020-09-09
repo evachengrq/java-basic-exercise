28d83b23edd7bf76beb3decd4efda844c2b29c86
程若前 <evachengrq@hodowakamaes-MacBook-Air.local>
2020-09-09 20:20:37 +0800

    [evacheng]feat: should work with normal inputs

--- src/main/java/GrammarExercise.java
+++ src/main/java/GrammarExercise.java
@@ -1,18 +1,39 @@
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GrammarExercise {
     public static void main(String[] args) {
         //需要从命令行读入
-        String firstWordList = "";
-        String secondWordList = "";
+        String firstWordList = args[0];
+        String secondWordList = args[1];
 
         List<String> result = findCommonWordsWithSpace(firstWordList,secondWordList);
         //按要求输出到命令行
-
+        System.out.println(result);
     }
 
     public static List<String> findCommonWordsWithSpace(String firstWordList, String secondWordList) {
         //在这编写实现代码
-        return null;
+        String[] array1 = firstWordList.split(",");//["apple","juice","mother","people","beautiful","apple","dog"]
+        String[] array2 = secondWordList.split(",");//["cat","baby","smile","good","apple","beautiful","Dog","nice"]
+
+        List<String> list1 = Arrays.asList(array1).stream()
+                .distinct()
+                .map(word -> word.toUpperCase())
+                .collect(Collectors.toList());
+        List<String> list2 = Arrays.asList(array2).stream()
+                .distinct()
+                .map(word -> word.toUpperCase())
+                .collect(Collectors.toList());
+
+        List<String> overlapped = list1.stream()
+                .filter(word -> list2.contains(word))
+                .collect(Collectors.toList());
+
+        return overlapped.stream()
+                .map(word -> String.join(" ", word.split("")))
+                .sorted()
+                .collect(Collectors.toList());
     }
 }
